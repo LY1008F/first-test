09c9a6a168737414201ba02c11f4413384c67af2
LY1008F <2411770181@qq.com>
2022-02-19 16:56:05 +0800

    数据校验

---
+++ springboot-1-config/src/main/java/com/yu/Springboot1ConfigApplication.java
@@ -0,0 +1,13 @@
+package com.yu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot1ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot1ConfigApplication.class, args);
+    }
+
+}
